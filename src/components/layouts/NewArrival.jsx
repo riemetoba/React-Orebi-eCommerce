ab3117d9d5b83e4480d68bb88619e712fd650954
Toba <riemehasan06@gmail.com>
2025-12-23 20:28:47 +0600

    API

--- src/components/layouts/NewArrival.jsx
+++ src/components/layouts/NewArrival.jsx
@@ -8,9 +8,19 @@ import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import NextArrow from "../NextArrow";
 import PrevArrow from "../PrevArrow";
+import { useEffect, useState } from "react";
+import axios from "axios";
 //import "slick-carousel/slick/slick-theme.css";
 
 const NewArrival = () => {
+  let [allData, setAllData] = useState([]);
+  useEffect(() => {
+    async function alldatas() {
+      let data = await axios.get("https://dummyjson.com/products");
+      setAllData(data.data.products);
+    }
+    alldatas();
+  });
   var settings = {
     dots: false,
     infinite: true,
@@ -26,58 +36,61 @@ const NewArrival = () => {
         <h2 className="text-[39px] font-DM font-bold pb-[55px]">
           New Arrivals
         </h2>
-       <div className="-mx-4">
-         <Slider {...settings}>
-          <div>
-            <Product
-              productImg={newArrivalsOne}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-          <div>
-            <Product
-              productImg={newArrivalsTwo}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-          <div>
-            <Product
-              productImg={newArrivalsThree}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-          <div>
-            <Product
-              productImg={newArrivalsFour}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-          <div>
-            <Product
-              productImg={newArrivalsOne}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-          <div>
-            <Product
-              productImg={newArrivalsTwo}
-              badgeText={"New"}
-              productTitle={"Basic Crew Neck Tee"}
-              productPrice={"$44.00"}
-            />
-          </div>
-        </Slider>
-       </div>
+        <div className="-mx-4">
+          <Slider {...settings}>
+            {allData.map((item) => (
+              <div>
+                <Product
+                  productImg={item.thumbnail}
+                  badgeText={item.rating}
+                  productTitle={item.title}
+                  productPrice={item.price}
+                />
+              </div>
+            ))}
+
+            <div>
+              <Product
+                productImg={newArrivalsTwo}
+                badgeText={"New"}
+                productTitle={"Basic Crew Neck Tee"}
+                productPrice={"$44.00"}
+              />
+            </div>
+            <div>
+              <Product
+                productImg={newArrivalsThree}
+                badgeText={"New"}
+                productTitle={"Basic Crew Neck Tee"}
+                productPrice={"$44.00"}
+              />
+            </div>
+            <div>
+              <Product
+                productImg={newArrivalsFour}
+                badgeText={"New"}
+                productTitle={"Basic Crew Neck Tee"}
+                productPrice={"$44.00"}
+              />
+            </div>
+            <div>
+              <Product
+                productImg={newArrivalsOne}
+                badgeText={"New"}
+                productTitle={"Basic Crew Neck Tee"}
+                productPrice={"$44.00"}
+              />
+            </div>
+            <div>
+              <Product
+                productImg={newArrivalsTwo}
+                badgeText={"New"}
+                productTitle={"Basic Crew Neck Tee"}
+                productPrice={"$44.00"}
+              />
+            </div>
+          </Slider>
+        </div>
       </Container>
     </>
   );
